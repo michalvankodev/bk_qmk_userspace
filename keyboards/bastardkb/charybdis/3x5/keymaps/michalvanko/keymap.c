2507959ee23eccc33d90e888dfc2038a1c5901ad
Michal Vanko <mklakakilli@gmail.com>
2026-01-07 22:35:37 +0100

    my dqts

--- keyboards/bastardkb/charybdis/3x5/keymaps/michalvanko/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/michalvanko/keymap.c
@@ -160,7 +160,7 @@ static uint16_t auto_pointer_layer_timer = 0;
  */
 #define LAYOUT_LAYER_SYMBOLS                                                                  \
     KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, _______________DEAD_HALF_ROW_______________, \
-    KC_COLN,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS, ______________HOME_ROW_GACS_R______________, \
+    KC_DQT,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS, ______________HOME_ROW_GACS_R______________, \
     KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE, _______________DEAD_HALF_ROW_______________, \
                       KC_LPRN, KC_RPRN, KC_UNDS, _______, XXXXXXX
 
