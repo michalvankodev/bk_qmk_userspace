9dcd5355474965e4fa679cd9143a5f30ea988c2b
Michal Vanko <mklakakilli@gmail.com>
2026-01-22 22:22:08 +0100

    fix being able to get back to layout from right hand side

--- keyboards/bastardkb/charybdis/3x5/keymaps/michalvanko/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/michalvanko/keymap.c
@@ -227,19 +227,19 @@ static uint16_t auto_pointer_layer_timer = 0;
        KC_LALT,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
        KC_LSFT,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SEMICOLON, \
        KC_LCTL,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
-                      MO(LAYER_GAMEPAD_L1), SPC_NAV, MO(LAYER_GAMEPAD_L2), ENT_SYM, BSP_NUM
+                      MO(LAYER_GAMEPAD_L1), SPC_NAV, MO(LAYER_GAMEPAD_L2), ENT_SYM, MO(LAYER_GAMEPAD_L2)
 
 #define LAYOUT_LAYER_GAMEPAD_L1                                                                     \
        KC_TRNS,    KC_7,    KC_TRNS,    KC_9,    KC_ESC,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TAB,    KC_H,    KC_J,    KC_K,    KC_L, KC_SEMICOLON, \
        KC_TRNS,    KC_1,    KC_2,    KC_3,    KC_0,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
-                      MO(LAYER_GAMEPAD_L1), SPC_NAV, MO(LAYER_GAMEPAD_L2), ENT_SYM, BSP_NUM
+                      MO(LAYER_GAMEPAD_L1), SPC_NAV, XXXXXXX, ENT_SYM, BSP_NUM
 
 #define LAYOUT_LAYER_GAMEPAD_L2                                                                     \
        KC_TRNS,    KC_F7,    KC_TRNS,    KC_F9,    KC_ESC,    KC_Y,    KC_U,    KC_I,    KC_O,    TG(LAYER_GAMEPAD_BASE), \
        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TAB,    KC_H,    KC_J,    KC_K,    KC_L, KC_SEMICOLON, \
        KC_TRNS,    KC_F1,    KC_F2,    KC_F3,    KC_0,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
-                      MO(LAYER_GAMEPAD_L1), SPC_NAV, MO(LAYER_GAMEPAD_L2), ENT_SYM, MO(LAYER_GAMEPAD_L2)
+                      XXXXXXX, SPC_NAV, MO(LAYER_GAMEPAD_L2), ENT_SYM, MO(LAYER_GAMEPAD_L2)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_wrapper(
